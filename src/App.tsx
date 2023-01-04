ecf6fbc61cf70df7589fcf9c77e33770446745a8
digitrom <roma.bobrovski@gmail.com>
2023-01-04 19:59:42 +0300

    End of lesson - 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <header className='header'>
-                <img
-                    src='https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium'/>
-            </header>
-            <nav className='nav'>
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-
-            </nav>
-            <div className='content'>
-                <div>
-                    <img
-                        src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New Post
-                    </div>
-                    <div>
-                        <div>Post 1
-                        </div>
-                        <div>Post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>);
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <header className='header'>
+            <img
+                src='https://www.adobe.com/express/create/media_127a4cd0c28c2753638768caf8967503d38d01e4c.jpeg?width=400&format=jpeg&optimize=medium'/>
+        </header>
+    );
+}
+
+export default Navbar;
