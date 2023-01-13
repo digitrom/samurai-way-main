aea78e47d5cdc980c73846a3d973efa8bad8d5a5
digitrom <roma.bobrovski@gmail.com>
2023-01-13 16:15:56 +0300

    End of lesson - 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,10 +3,28 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-    <div>
-        Dialogs
-    </div>
-)
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Roma
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Vlad
+                </div>
+                <div className={s.dialog}>
+                    Pasha
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hey</div>
+                <div className={s.dialog}>How are You</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs
