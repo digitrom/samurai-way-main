a6ac5bc668161f00f8a980142ca5ad66f9afa53b
digitrom <roma.bobrovski@gmail.com>
2023-08-25 23:30:44 +0200

    Status.tsx - status line in ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import {ProfilePageType, ProfileType} from "../../../redux/profile-reducer";
 import Preloader from "../../common/preloader/Preloader";
+import Status from "../../Status";
 
 export type ProfileInfoPropsType = {
     profile: ProfileType | null
@@ -18,6 +19,7 @@ const ProfileInfo = (props:ProfileInfoPropsType) => {
                 <img
                     src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'/>
             </div>
+            <Status/>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
                 ava + description
