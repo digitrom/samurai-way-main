d04b74e9612fad934f4cdbaba9d7b1560a4cb220
digitrom <roma.bobrovski@gmail.com>
2024-01-13 19:49:20 +0100

    lesson 90 - refactoring end 1

--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -32,7 +32,7 @@ export type InitialStateType = {
 let initialState: InitialStateType = {
     users: [],
     totalUsersCount: 0,
-    pageSize: 5,
+    pageSize: 10,
     currentPage: 5,
     isFetching: true
 }
